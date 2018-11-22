65a8db22afcd6c0c0a40b73bd76ee8bbf31f40a9
leon in 't Veld <leon3110l@gmail.com>
2018-11-22 22:59:47 +0100

    added svg

--- src/views/ProjectsView.js
+++ src/views/ProjectsView.js
@@ -1,18 +1,20 @@
 import React from "react"
 
 import Page from '../components/Page'
-import {Row} from "../components/Grid"
-import Project from "../components/Project"
-import projects from "../projects.json"
+import wave from "../assets/page_transition.svg"
+// import {Row} from "../components/Grid"
+// import Project from "../components/Project"
+// import projects from "../projects.json"
 
 export default class ProjectsView extends React.Component {
     render() {
         return (
             <Page>
-                <h1>Projects</h1>
+                <img src={wave} alt="wave"/>
+                {/* <h1>Projects</h1>
                 <Row>
                     {projects.map((e, i) => <Project xs={12} sm={6} md={4} lg={3} key={i} data={e}/>)}
-                </Row>
+                </Row> */}
             </Page>
         )
     }
